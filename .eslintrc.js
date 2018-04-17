21434a7124cfab2d857af94b3f1ebb0d0abf1872
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2018-04-17 13:01:16 -0500

    version bump

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
     "extends": "airbnb",
+    "env": {
+        "jest": true,
+    },
     "rules": {
         'no-use-before-define': 0,
         'react/jsx-filename-extension': 0,
