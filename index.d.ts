296f18b432147abda9ae642860eebdedc32144b1
Thomas Chia <thomas.h.chia@googlemail.com>
2018-09-05 09:42:30 +0800

    Add types file. (#74)
    * Add type file.
    
    * Update type for value.

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="react" />
+
+declare module 'react-native-picker-select' {
+    export interface Item {
+        label: string;
+        value: any;
+        key?: string;
+        color?: string;
+    }
+    type ModeOptions = 'dialog' | 'dropdown';
+    export interface PickerProps {
+        onValueChange: (value: any, index: number) => void;
+        items: Item[];
+        placeholder?: Item | {};
+        disabled?: boolean;
+        value?: any;
+        itemKey?: string | number;
+        style?: object;
+        hideIcon?: boolean;
+        hideDoneBar?: boolean;
+        onUpArrow?: () => void;
+        onDownArrow?: () => void;
+        mode?: ModeOptions;
+    }
+    class Picker extends React.Component<PickerProps> {}
+    export default Picker;
+}
