7daf888e96c1c41ff099756456b2957238eedddc
Arno Lehtonen <arno.lehtonen@gofore.com>
2019-11-02 16:23:33 +0200

    Improve Typescript type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
-// / <reference types="react" />
+import { ViewStyle, TextStyle, ModalProps, TextInputProperties, PickerProps } from 'react-native';
+import React from 'react';
 
 declare module 'react-native-picker-select' {
     export interface Item {
@@ -7,30 +8,63 @@ declare module 'react-native-picker-select' {
         key?: string | number;
         color?: string;
     }
-    export interface PickerProps {
+    export interface PickerStyle {
+        chevron?: ViewStyle;
+        chevronActive?: ViewStyle;
+        chevronContainer?: ViewStyle;
+        chevronDown?: ViewStyle;
+        chevronUp?: ViewStyle;
+        done?: TextStyle;
+        headlessAndroidContainer?: ViewStyle;
+        headlessAndroidPicker?: ViewStyle;
+        iconContainer?: ViewStyle;
+        inputAndroid?: TextStyle;
+        inputAndroidContainer?: ViewStyle;
+        inputIOS?: TextStyle;
+        inputIOSContainer?: ViewStyle;
+        modalViewBottom?: ViewStyle;
+        modalViewMiddle?: ViewStyle;
+        modalViewTop?: ViewStyle;
+        placeholder?: TextStyle;
+        viewContainer?: ViewStyle;
+    }
+
+    // Omit props needed by the library
+    type PickerModalProps = Omit<
+        ModalProps,
+        | 'testID'
+        | 'visible'
+        | 'transparent'
+        | 'animationType'
+        | 'supportedOrientations'
+        | 'onDismiss'
+        | 'onOrientationChange'
+    >;
+
+    export interface PickerSelectProps {
         onValueChange: (value: any, index: number) => void;
         items: Item[];
         value?: any;
         placeholder?: Item | {};
         disabled?: boolean;
         itemKey?: string | number;
-        style?: object;
-        children?: any;
-        placeholderTextColor?: string; // deprecated
+        style?: PickerStyle;
+        children?: React.ReactNode;
+        placeholderTextColor?: string; // deprecated
         useNativeAndroidPickerStyle?: boolean;
-        hideDoneBar?: boolean; // deprecated
+        hideDoneBar?: boolean; // deprecated
         doneText?: string;
         onDonePress?: () => void;
         onUpArrow?: () => void;
         onDownArrow?: () => void;
         onOpen?: () => void;
         onClose?: () => void;
-        modalProps?: object;
-        textInputProps?: object;
-        pickerProps?: object;
+        modalProps?: PickerModalProps;
+        textInputProps?: TextInputProperties;
+        pickerProps?: PickerProps;
         Icon?: React.ReactNode;
         InputAccessoryView?: React.ReactNode;
     }
-    class Picker extends React.Component<PickerProps> {}
+    class Picker extends React.Component<PickerSelectProps> {}
     export default Picker;
 }
