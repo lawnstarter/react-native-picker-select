5a407a6e2960c184d7798f9fdfdd804ea7aa9201
Michael Lefkowitz <1746853+lfkwtz@users.noreply.github.com>
2020-06-03 09:16:09 -0500

    Update src/index.js
    Co-authored-by: Matt McDonald <matt@wildside.uk>

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {
 import PropTypes from 'prop-types';
 import isEqual from 'lodash.isequal';
 import { defaultStyles } from './styles';
-import { Picker } from "@react-native-community/picker";
+import { Picker } from '@react-native-community/picker';
 
 export default class RNPickerSelect extends PureComponent {
     static propTypes = {
