a4953d5586f74e45e0c09b7f5c823e4321081720
Christoph Pader <christoph@margelo.io>
2023-10-16 10:06:45 +0200

    chore: add jest support to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: ['plugin:react/recommended', 'airbnb', 'airbnb/hooks', 'prettier'],
     plugins: ['react', 'react-native'],
